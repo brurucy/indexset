4a011958eaf07da2a3f074bad7a4f215ee7372e0
brurucy <brurucy@gmail.com>
2024-12-01 17:04:57 +0200

    Comitting system added.

--- src/concurrent2/set.rs
+++ src/concurrent2/set.rs
@@ -7,13 +7,15 @@ use parking_lot::{Mutex, RwLock};
 use crate::core::constants::DEFAULT_INNER_SIZE;
 use crate::core::node::*;
 
+type Node<T> = Arc<Mutex<Vec<T>>>;
+
 #[derive(Debug)]
 pub struct BTreeSet<T>
 where
     T: Ord + Clone + 'static,
 {
     node_capacity: usize,
-    index: SkipMap<T, Arc<Mutex<Vec<T>>>>,
+    index: SkipMap<T, Node<T>>,
     index_lock: RwLock<()>,
 }
 impl<T: Ord + Clone + 'static> Default for BTreeSet<T> {
@@ -29,71 +31,143 @@ impl<T: Ord + Clone + 'static> Default for BTreeSet<T> {
     }
 }
 
+type NewVersion<T> = Node<T>;
+type OldVersion<T> = Node<T>;
+type CurrentVersion<T> = Node<T>;
+
+enum Operation<T> {
+    Split(OldVersion<T>, T, T),
+    UpdateMax(CurrentVersion<T>, T),
+}
+
+impl<T: Ord + Send + Clone + 'static> Operation<T> {
+    fn commit(self, index: &SkipMap<T, Node<T>>) -> bool {
+        match self {
+            Operation::Split(old_node, old_max, value) => {
+                let mut guard = old_node.lock();
+                if let Some(entry) = index.get(&old_max) {
+                    if Arc::ptr_eq(entry.value(), &old_node) {
+                        index.remove(&old_max);
+
+                        let mut new_vec = guard.halve();
+
+                        let mut inserted = false;
+                        let mut insert_attempted = false;
+                        if let Some(max) = guard.last().cloned() {
+                            if max >= value {
+                                inserted = NodeLike::insert(&mut *guard, value.clone());
+                                insert_attempted = true;
+                            }
+
+                            index.insert(max, old_node.clone());
+                        }
+
+                        if let Some(max) = new_vec.last().cloned() {
+                            if !insert_attempted {
+                                inserted = NodeLike::insert(&mut new_vec, value.clone());
+                            }
+
+                            index.insert(max, Arc::new(Mutex::new(new_vec)));
+                        }
+
+                        return inserted;
+                    }
+                }
+
+                false
+            }
+            Operation::UpdateMax(node, old_max) => {
+                let guard = node.lock();
+                let new_max = guard.last().unwrap();
+                if let Some(entry) = index.get(&old_max) {
+                    if Arc::ptr_eq(entry.value(), &node) {
+                        match new_max.cmp(&old_max) {
+                            std::cmp::Ordering::Less => panic!("new_max is smaller than old_max?"),
+                            std::cmp::Ordering::Equal => panic!("new_max is equal to old_max?"),
+                            std::cmp::Ordering::Greater => {
+                                index.remove(&old_max);
+                                index.insert(new_max.clone(), node.clone());
+
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                false
+            }
+        }
+    }
+}
+
 impl<T: Ord + Clone + Send + Debug> BTreeSet<T> {
     pub fn new() -> Self {
         Self::default()
     }
     pub fn insert(&self, value: T) -> bool {
         loop {
-            let global_guard = self.index_lock.read();
-            let target_node = match self.index.lower_bound(std::ops::Bound::Included(&value)) {
-                Some(entry) => entry.value().clone(),
-                None => self
-                    .index
-                    .back()
-                    .map(|last| last.value().clone())
-                    .or_else(|| Some(Arc::new(Mutex::new(Vec::with_capacity(self.node_capacity)))))
-                    .unwrap(),
+            let mut _global_guard = self.index_lock.upgradable_read();
+            let target_node_entry = match self.index.lower_bound(std::ops::Bound::Included(&value))
+            {
+                Some(entry) => entry,
+                None => {
+                    if let Some(last) = self.index.back() {
+                        last
+                    } else {
+                        let mut first_vec = Vec::with_capacity(self.node_capacity);
+                        first_vec.push(value.clone());
+
+                        let first_node = Arc::new(Mutex::new(first_vec));
+
+                        if let Some(_) = _global_guard
+                            .try_with_upgraded(|_| self.index.insert(value.clone(), first_node))
+                        {
+                            return true;
+                        }
+
+                        continue;
+                    }
+                }
             };
 
-            let mut node_guard = target_node.lock();
-            if node_guard.len() >= self.node_capacity {
-                if NodeLike::insert(&mut *node_guard, value.clone()) {
-                    drop(global_guard);
-                    drop(node_guard);
-                    let _global_guard = self.index_lock.write();
-                    let mut node_guard = target_node.lock();
-
-                    if let Some(old_max) = node_guard.last().cloned() {
-                        self.index.remove(&old_max);
-                    }
-                    let new_node_vec = node_guard.halve();
-                    if let Some(max) = node_guard.last().cloned() {
-                        self.index.insert(max, target_node.clone());
-                    }
-
-                    let new_node = Arc::new(Mutex::new(new_node_vec));
-                    if let Some(max) = new_node.lock().last().cloned() {
-                        self.index.insert(max, new_node.clone());
-                    }
-
-                    return true;
-                };
-
-                break;
-            } else {
+            let mut node_guard = target_node_entry.value().lock();
+            let mut operation = None;
+            if node_guard.len() < self.node_capacity {
                 let old_max = node_guard.last().cloned();
                 let inserted = NodeLike::insert(&mut *node_guard, value.clone());
                 if inserted {
-                    if let Some(new_max) = node_guard.last().cloned() {
-                        if Some(&new_max) != old_max.as_ref() {
-                            drop(global_guard);
-                            drop(node_guard);
-                            let _global_guard = self.index_lock.write();
-                            if let Some(old_max) = old_max {
-                                self.index.remove(&old_max);
-                            }
-
-                            self.index.insert(new_max, target_node.clone());
-                        }
+                    if node_guard.last().cloned() == old_max {
+                        return true;
                     }
+
+                    if old_max.is_some() {
+                        operation = Some(Operation::UpdateMax(
+                            target_node_entry.value().clone(),
+                            old_max.unwrap(),
+                        ))
+                    }
+                } else {
+                    return false;
                 }
-
-                return inserted;
+            } else {
+                operation = Some(Operation::Split(
+                    target_node_entry.value().clone(),
+                    target_node_entry.key().clone(),
+                    value.clone(),
+                ))
             }
-        }
 
-        false
+            drop(_global_guard);
+            drop(node_guard);
+            let _global_guard = self.index_lock.write();
+
+            if operation.unwrap().commit(&self.index) {
+                return true;
+            }
+            drop(_global_guard);
+
+            continue;
+        }
     }
     fn locate_node<Q>(&self, value: &Q) -> Option<Arc<Mutex<Vec<T>>>>
     where
@@ -139,7 +213,7 @@ mod tests {
     #[test]
     fn test_concurrent_insert() {
         let set = Arc::new(BTreeSet::<i32>::new());
-        let num_threads = 16;
+        let num_threads = 128;
         let operations_per_thread = 10000;
         let mut handles = vec![];
 
@@ -191,10 +265,11 @@ mod tests {
         let set = Arc::new(BTreeSet::<i32>::new());
         let mut rng = rand::thread_rng();
 
-        let range = 0..100000;
+        let n = 2048 * 100;
+        let range = 0..n;
         let mut inserted_values = HashSet::new();
         for _ in range {
-            let value = rng.gen_range(0..100000);
+            let value = rng.gen_range(0..n);
             if inserted_values.insert(value) {
                 assert!(set.insert(value));
             }
@@ -203,8 +278,13 @@ mod tests {
         assert_eq!(
             set.len(),
             inserted_values.len(),
-            "Length did not match: {:?}",
-            set.index.iter().collect::<Vec<_>>()
+            "Length did not match, missing: {:?}",
+            set.index
+                .iter()
+                .flat_map(|entry| entry.value().lock().iter().cloned().collect::<Vec<_>>())
+                .collect::<HashSet<_>>()
+                .symmetric_difference(&inserted_values)
+                .collect::<Vec<_>>()
         );
         for i in inserted_values {
             assert!(
